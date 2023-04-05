a66154acb458c6961951165b9ef55a61d93ff1a1
maruakinu <marloaquino080621@gmail.com>
2023-04-05 20:58:27 +0800

    Project Added

---
+++ src/main/java/com/springboot/crud/mysqlspring/MysqlSpringApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.crud.mysqlspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysqlSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/crud/mysqlspring/MysqlSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.crud.mysqlspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysqlSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
