602e810a7cb1f33f24a341103c1bacbaa788b783
maruakinu <marloaquino080621@gmail.com>
2023-05-10 23:23:55 +0800

    RImplementation of JUnit

--- src/test/java/com/springboot/crud/mysqlspring/ArticleServiceImplTest.java
+++ src/test/java/com/springboot/crud/mysqlspring/ArticleServiceImplTest.java
@@ -5,18 +5,23 @@ import com.springboot.crud.mysqlspring.domain.article.entity.ArticleEntity;
 import com.springboot.crud.mysqlspring.domain.article.repository.ArticleRepository;
 import com.springboot.crud.mysqlspring.domain.article.service.ArticleServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class ArticleServiceImplTest {
-
     ArticleServiceImpl articleService;
 
     @Mock
     ArticleRepository articleRepository;
-
 
     private ArticleDto article;
 
@@ -43,7 +48,18 @@ public class ArticleServiceImplTest {
 
     }
 
+    @Test
+    void whenValidArticleForm_thenReturnArticle() {
+        String wrongTitle = "wrong";
+        // when(articleRepository.save(any(ArticleEntity.class))).thenReturn(expectedArticle);
 
+
+        // ArticleDto actual =  articleService.createArticle(article);
+
+        assertEquals(expectedTitle, article.getTitle());
+        assertNotEquals(expectedTitle, wrongTitle);
+
+    }
 
 
 }
