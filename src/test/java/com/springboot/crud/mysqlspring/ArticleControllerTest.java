cc3c1b0d7a8f6238debd6f0e5999831dede990ff
maruakinu <marloaquino080621@gmail.com>
2023-05-10 22:09:28 +0800

    Refactoring of Test code

--- src/test/java/com/springboot/crud/mysqlspring/ArticleControllerTest.java
+++ src/test/java/com/springboot/crud/mysqlspring/ArticleControllerTest.java
@@ -1,37 +1,22 @@
 package com.springboot.crud.mysqlspring;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.springboot.crud.mysqlspring.domain.article.entity.ArticleEntity;
 import com.springboot.crud.mysqlspring.domain.article.controller.ArticlesController;
 import com.springboot.crud.mysqlspring.domain.article.dto.ArticleDto;
 import com.springboot.crud.mysqlspring.domain.article.service.ArticleService;
 import org.hamcrest.Matchers;
-import org.junit.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.mockito.ArgumentMatchers;
-import org.mockito.Mockito;
-import org.mockito.stubbing.Answer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
-import javax.validation.constraints.Null;
 import java.util.List;
-import java.util.Optional;
-
-import static org.hamcrest.Matchers.*;
-import static org.hamcrest.Matchers.hasItem;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.isNull;
 import static org.mockito.Mockito.when;
 import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
