49e1b33e5fba81462a37d2465155621398683dc9
maruakinu <marloaquino080621@gmail.com>
2023-08-21 12:00:49 +0800

    Updating Dev and Prod Properties

--- src/main/java/com/springboot/crud/mysqlspring/domain/user/entity/UserEntity.java
+++ src/main/java/com/springboot/crud/mysqlspring/domain/user/entity/UserEntity.java
@@ -1,4 +1,15 @@
 package com.springboot.crud.mysqlspring.domain.user.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
 public class UserEntity {
 }
